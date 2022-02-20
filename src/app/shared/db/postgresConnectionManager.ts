fce6170a4dc1d0060ce3b93447f03cb4910488ae
michalovsky <michalovskyyy@gmail.com>
2022-02-20 21:50:24 +0100

    fix multiple connections to db issue

--- src/app/shared/db/postgresConnectionManager.ts
+++ src/app/shared/db/postgresConnectionManager.ts
@@ -2,7 +2,13 @@ import { Book } from '../../domain/book/entities/book';
 import { createConnection } from 'typeorm';
 
 export class PostgresConnectionManager {
+  private static connected = false;
+
   public static async connect(): Promise<void> {
+    if (PostgresConnectionManager.connected) {
+      return;
+    }
+
     await createConnection({
       type: 'postgres',
       host: process.env.DB_HOST,
@@ -14,6 +20,7 @@ export class PostgresConnectionManager {
       synchronize: true,
     });
 
+    PostgresConnectionManager.connected = true;
     console.log(`Connected to ${process.env.DB_NAME} database on port ${process.env.DB_PORT}`);
   }
 }
