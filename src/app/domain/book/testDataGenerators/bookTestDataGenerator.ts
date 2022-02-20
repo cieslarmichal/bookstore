8df75a4a96ca031eb054bd5aa9daf081eef3cc52
michalovsky <michalovskyyy@gmail.com>
2022-02-20 15:19:14 +0100

    add book test data generator

--- src/app/domain/book/testDataGenerators/bookTestDataGenerator.ts
+++ src/app/domain/book/testDataGenerators/bookTestDataGenerator.ts
@@ -1,24 +1,62 @@
 import { Book } from '../entities/book';
 import { Faker } from '@faker-js/faker';
+import { BookFormat, BookLanguage } from '../types';
 
 export class BookTestDataGenerator {
   private faker = new Faker();
 
-  public generateData(): Book {}
+  public generateData(): Book {
+    return {
+      id: this.generateId(),
+      createdAt: this.generateCreatedAt(),
+      updatedAt: this.generateUpdatedAt(),
+      title: this.generateTitle(),
+      author: this.generateAuthor(),
+      releaseYear: this.generateReleaseYear(),
+      language: this.generateLanguage(),
+      format: this.generateFormat(),
+      description: this.generateDescription(),
+      price: this.generatePrice(),
+    };
+  }
 
   public generateId(): number {
     return this.faker.datatype.number();
   }
 
   public generateCreatedAt(): Date {
-    return date.recent(3);
+    return this.faker.date.recent(3);
   }
 
   public generateUpdatedAt(): Date {
-    return date.recent(1);
+    return this.faker.date.recent(1);
   }
 
   public generateTitle(): string {
-    return lorem.text(10);
+    return this.faker.lorem.text(10);
+  }
+
+  public generateAuthor(): string {
+    return this.faker.name.firstName() + ' ' + this.faker.name.lastName();
+  }
+
+  public generateReleaseYear(): number {
+    return this.faker.datatype.number({ min: 1000, max: 2100 });
+  }
+
+  public generateLanguage(): BookLanguage {
+    return this.faker.helpers.randomize([BookLanguage.en, BookLanguage.pl]);
+  }
+
+  public generateFormat(): BookFormat {
+    return this.faker.helpers.randomize([BookFormat.hardcover, BookFormat.paperback, BookFormat.paperback]);
+  }
+
+  public generateDescription(): string {
+    return this.faker.lorem.text(50);
+  }
+
+  public generatePrice(): number {
+    return this.faker.datatype.number({ min: 1, max: 1000 });
   }
 }
