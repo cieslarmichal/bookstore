c3b44c74ba857a0ffb3e61ef6b2ae51b9b8a2c11
michalovsky <michalovskyyy@gmail.com>
2022-02-22 22:52:07 +0100

    remove testing env

--- src/app/config/configLoader.ts
+++ src/app/config/configLoader.ts
@@ -3,8 +3,6 @@ import path from 'path';
 
 export class ConfigLoader {
   public static loadConfig() {
-    const envFileName = process.env.NODE_ENV === 'test' ? '.env.testing' : '.env';
-
-    dotenv.config({ path: path.resolve(__dirname, `../../../${envFileName}`) });
+    dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
   }
 }
