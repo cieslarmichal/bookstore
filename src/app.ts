f2588474ca8ddc7afcd78bef396ab7a44895cd70
michalovsky <michalovskyyy@gmail.com>
2022-03-01 22:25:35 +0100

    add json middleware

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { AuthorModule } from './app/domain/author/authorModule';
 import { DbModule } from './app/shared/db/dbModule';
 import { ControllersModule } from './app/controllers/controllersModule';
 import helmet from 'helmet';
+import { jsonMiddleware } from './app/middlewares/jsonMiddleware';
 
 export class App {
   public expressApp: express.Application;
@@ -19,9 +20,10 @@ export class App {
   private async setup() {
     ConfigLoader.loadConfig();
 
+    this.expressApp.use(helmet());
     this.expressApp.use(express.json());
     this.expressApp.use(express.urlencoded({ extended: false }));
-    this.expressApp.use(helmet());
+    this.expressApp.use(jsonMiddleware);
 
     const container = await createDIContainer([DbModule, BookModule, AuthorModule, ControllersModule]);
 
