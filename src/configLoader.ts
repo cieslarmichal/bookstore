975517cfac559890f571a683f128661961f588af
michalovsky <michalovskyyy@gmail.com>
2022-03-10 00:46:51 +0100

    fix config loading

--- src/configLoader.ts
+++ src/configLoader.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
-import path from 'path';
 
 export class ConfigLoader {
   public static loadConfig() {
-    dotenv.config({ path: path.resolve(__dirname, '.env') });
+    dotenv.config();
   }
 }
