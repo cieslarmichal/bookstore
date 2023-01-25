bbd070f64f90fb0bf9b67d95cca4ad499a0d6090
Michal Cieslar <michalovskyyy@gmail.com>
2023-01-25 17:15:32 +0100

    add http media type enum

---
+++ src/app/common/http/httpMediaType.ts
@@ -0,0 +1,7 @@
+export enum HttpMediaType {
+  applicationJson = 'application/json',
+  formUrlEncoded = 'application/x-www-form-urlencoded',
+  textXml = 'text/xml',
+  textCsv = 'text/csv',
+  textPlain = 'text/plain',
+}
