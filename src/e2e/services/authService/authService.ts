a06e4aec7b52fb834267e5ad60ede5654a481925
Michal Cieslar <michalovskyyy@gmail.com>
2023-06-17 14:02:04 +0200

    add auth test service

---
+++ src/e2e/services/authService/authService.ts
@@ -0,0 +1,24 @@
+import {
+  LoginUserBody,
+  LoginUserResponseOkBody,
+} from '../../../application/modules/userModule/api/httpControllers/userHttpController/schemas/loginUserSchema';
+import { HttpMethodName } from '../../../common/http/httpMethodName';
+import { HttpService } from '../../../libs/http/services/httpService/httpService';
+
+export class AuthService {
+  public constructor(private readonly httpService: HttpService) {}
+
+  public async getUserToken(loginUserBody: LoginUserBody): Promise<string> {
+    const response = await this.httpService.sendRequest<LoginUserResponseOkBody>({
+      method: HttpMethodName.post,
+      endpoint: 'users/login',
+      body: loginUserBody,
+    });
+
+    if (!response.isSuccess) {
+      throw new Error();
+    }
+
+    return response.body.token;
+  }
+}
